ef76914407fcc7490b7634ab46235e12f82c14cd
Pablo Reyes <pablo@rhodium.cl>
2017-11-18 21:09:06 -0300

    nuevaversionrutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :directorios
   #root :to => 'directories#index'
-  root directorios#index
+  #root directorio#index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
