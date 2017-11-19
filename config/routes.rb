028cd4a5e88974c9dd435799e0cefc22348f3ad3
Pablo Reyes <pablo@rhodium.cl>
2017-11-19 00:00:06 -0300

    intentorutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :directorios
+  root 'directorios#index'
   #root :to => 'directories#index'
   #root directorio#index
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
